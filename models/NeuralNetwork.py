b33d98930fe6e4989a31dd91339819da7a87143d
Guilherme Tassianri <guitassinari@gmail.com>
2019-06-07 21:37:28 -0300

    calculates deltas

--- models/NeuralNetwork.py
+++ models/NeuralNetwork.py
@@ -15,39 +15,61 @@ class NeuralNetwork:
         self.weight_matrices = []
         self.bias_weights_matrices = []
         self._lambda = hyper_parameters["lambda"] or 0.1
+        self.last_activations = [None] * self.n_layers
+        self.deltas = [None] * self.n_layers
         self.build_neurons()
         self.train(training_set)
-        self.last_activations = []
 
     def predict(self, features=[]):
         """
         :param features:
         :return:
         """
-        return self.outputs(features).tolist()
+        return self.outputs(features)
 
-    def outputs(self, features=[]):
-        accumulator = features
+    def outputs(self, inputs=[]):
+        features_matrix = self.array_to_matrix(inputs)
+        accumulator = features_matrix
         # Multiplica todas as matrizes, (entrada x pesos) + bias.
         # Forward propagation
         for layer_i in range(len(self.weight_matrices)):
             accumulator = self.hidden_activation(accumulator, layer_i)
-        return accumulator
+        return accumulator.tolist()
 
-    def hidden_activation(self, acc=[], layer=0):
-        accumulator = np.array(acc)
+    def hidden_activation(self, acc_matrix=[], layer=0):
         weights = self.weight_matrices[layer]
         bias = self.bias_weights_matrices[layer]
-        zs = np.add(accumulator.dot(weights), bias)
+        print(weights, acc_matrix, bias)
+        zs = np.add(weights.dot(acc_matrix), bias)
+        print(zs)
         activations = NeuralNetworkMath.sigmoid(zs)
         self.last_activations[layer] = activations
         return activations
+
+    def back_propagate(self, expected_outputs=[]):
+        for layer in reversed(range(self.n_layers)):
+            if layer == self.last_layer_index():
+                deltas = self.outputs(expected_outputs)
+            else:
+                activations = self.last_activations[layer]
+                weights = self.weight_matrices[layer]
+                next_layer_deltas = self.deltas[layer+1]
+                deltas = NeuralNetworkMath.delta(activations,
+                                                 weights,
+                                                 next_layer_deltas)
+            self.deltas[layer] = deltas
+
+    def output_deltas(self, output_matrix=[[]]):
+        outputs = self.last_activations[self.last_layer_index()]
+        return np.subtract(outputs, output_matrix)
 
     def train(self, training_dataset):
         loss = 0
         for example in training_dataset.get_examples():
             # Isso tá incompleto. Ver a função NeuralNetworkMath.loss
-            loss += NeuralNetworkMath.loss(self.outputs(example.get_body()))
+            inputs = example.get_body()
+            outputs = self.outputs(inputs)
+            loss += NeuralNetworkMath.loss(outputs, [[1]])
         n_examples = len(training_dataset.examples)
         loss = loss / n_examples
         regularization = NeuralNetworkMath.loss_regularization(self.weight_matrices,
@@ -61,11 +83,16 @@ class NeuralNetwork:
                 continue
             previous_n_neurons = self.layer_neurons[layer - 1]
             n_neurons = self.layer_neurons[layer]
-            weights = InitialWeights.generate(previous_n_neurons,
-                                              n_neurons,
+            weights = InitialWeights.generate(n_neurons,
+                                              previous_n_neurons,
                                               debug=self.debug)
-            bias_weights = InitialWeights.generate(1, n_neurons, debug=self.debug)
+            bias_weights = InitialWeights.generate(n_neurons, 1, debug=self.debug)
             self.weight_matrices.append(np.array(weights))
             self.bias_weights_matrices.append(bias_weights)
 
+    def last_layer_index(self):
+        return self.n_layers-1
+
+    def array_to_matrix(self, array=[]):
+        return list(map(lambda inp: [inp], array))
 
--- tests/models/NeuralNetwork_test.py
+++ tests/models/NeuralNetwork_test.py
@@ -3,33 +3,43 @@
 from models.NeuralNetwork import NeuralNetwork
 from models.NeuralNetworkMath import NeuralNetworkMath
 from data.Dataset import Dataset
+from data.Example import Example
 
 inputs = 1
 outputs = 1
 hidden_layer_neurons = 3
+example = Example([1, 2], [1, 1])
+dataset = Dataset([example])
 parameters = {
-    "layers_structure": [inputs, hidden_layer_neurons, outputs]
+    "layers_structure": [inputs, hidden_layer_neurons, outputs],
+    "lambda": 0.1
 }
 
 def test_weight_matrices():
-    nn = NeuralNetwork(parameters, Dataset([]), debug=True)
+    nn = NeuralNetwork(parameters, dataset, debug=True)
     assert len(nn.weight_matrices) == 2  # input -> hidden, hidden -> output
-    assert len(nn.weight_matrices[0]) == inputs
-    assert len(nn.weight_matrices[1]) == hidden_layer_neurons
-    assert len(nn.weight_matrices[0][0]) == hidden_layer_neurons
-    assert len(nn.weight_matrices[1][0]) == outputs
+    assert len(nn.weight_matrices[0]) == hidden_layer_neurons
+    assert len(nn.weight_matrices[1]) == outputs
+    assert len(nn.weight_matrices[0][0]) == inputs
+    assert len(nn.weight_matrices[1][0]) == hidden_layer_neurons
 
 
 def test_prediction():
-    nn = NeuralNetwork(parameters, Dataset([]), debug=True)
+    nn = NeuralNetwork(parameters, dataset, debug=True)
     first_line = nn.predict([1])[0]
     assert first_line == [0.9744787489988975]
 
 
 def test_hidden_activations():
-    nn = NeuralNetwork(parameters, Dataset([]), debug=True)
+    nn = NeuralNetwork(parameters, dataset, debug=True)
     first_line = nn.hidden_activation([1])[0]
     expected_result = NeuralNetworkMath.sigmoid(2)
     expected_line = [expected_result, expected_result, expected_result]
-    print(first_line, expected_line)
     assert all([a == b for a, b in zip(first_line, expected_line)])
+
+
+def test_backpropagate():
+    nn = NeuralNetwork(parameters, dataset, debug=True)
+
+    nn.back_propagate([0])
+    assert nn.deltas[2][0] == [0.9605766589005886]
