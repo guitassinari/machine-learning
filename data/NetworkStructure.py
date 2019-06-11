9fa7b3fd437895be82c8d32287d36084bca2f9c9
tbvinicius <tbvinicius@hotmail.com>
2019-06-11 16:28:42 -0300

    Update NetworkStructure.py

--- data/NetworkStructure.py
+++ data/NetworkStructure.py
@@ -1,8 +1,13 @@
-
+import csv
 class NetworkStructure:
     def __init__(self, file_path):
         self.file_path = file_path
 
 
     def read(self):
-        pass
+        examples = []
+        with open(self.file_path) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter='\n')
+            for row in csv_reader:
+                examples.append(row)
+        return examples
